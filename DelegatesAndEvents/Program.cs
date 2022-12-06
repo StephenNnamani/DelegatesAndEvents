f8154e8d9c72d01518af69088e4541598764475e
StephenNnamani <sir.stephennnamani@gmail.com>
2022-12-06 06:43:00 -0500

    Initial commit

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DelegatesAndEvents
+{
+    public delegate void CustomDelegate(string msg);
+    internal class Program
+    {
+        static void Main()
+        {
+            CustomDelegate logDelegate = Logger.Log;
+            CustomDelegate notificationDelegate = Notification.Log;
+            CustomDelegate del = logDelegate + notificationDelegate;
+            del("Hello World!!!");
+
+            del = del - notificationDelegate;
+            del("Hello King!!!");
+        }
+    }
+    class Logger
+    {
+        public static void Log(string message)
+        {
+            Console.WriteLine($"I am Logger: {message}");
+        }
+    }
+
+    class Notification
+    {
+        public static void Log(string message)
+        { 
+            Console.WriteLine($"I am Notification: {message}");
+        }
+    }
+}
